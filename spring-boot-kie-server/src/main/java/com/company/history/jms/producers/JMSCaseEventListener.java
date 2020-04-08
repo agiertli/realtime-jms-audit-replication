e8acffc7d35cfcb9bc8bb0182fdd8509b4e7d203
Anton Giertli <you@example.com>
2020-04-08 14:35:30 +0200

    refactoring

--- spring-boot-kie-server/src/main/java/com/company/history/jms/producers/JMSCaseEventListener.java
+++ spring-boot-kie-server/src/main/java/com/company/history/jms/producers/JMSCaseEventListener.java
@@ -1,20 +1,19 @@
 package com.company.history.jms.producers;
 
+import static org.kie.soup.commons.xstream.XStreamUtils.createTrustingXStream;
+
+import javax.jms.TextMessage;
+
 import org.jbpm.casemgmt.impl.jms.AsyncCaseInstanceAuditEventProducer;
 import org.jbpm.casemgmt.impl.model.AuditCaseInstanceData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 import com.thoughtworks.xstream.XStream;
-
-import static org.kie.soup.commons.xstream.XStreamUtils.createTrustingXStream;
-
-import javax.jms.TextMessage;
 
 @Component
 public class JMSCaseEventListener extends AsyncCaseInstanceAuditEventProducer {
@@ -23,7 +22,7 @@ public class JMSCaseEventListener extends AsyncCaseInstanceAuditEventProducer {
 	@Autowired
 	private JmsTemplate jmsTemplate;
 	private XStream xstream;
-	
+
 	@Value("${audit.queue}")
 	private String queue;
 
@@ -41,8 +40,8 @@ public class JMSCaseEventListener extends AsyncCaseInstanceAuditEventProducer {
 
 		jmsTemplate.send(queue, messageCreator -> {
 			TextMessage message = messageCreator.createTextMessage(eventXml);
-            message.setIntProperty("EventType", eventType);
-            message.setStringProperty("LogType", "Case");
+			message.setIntProperty("EventType", eventType);
+			message.setStringProperty("LogType", "Case");
 			message.setJMSPriority(priority);
 			logger.info("Case Sending JMS Message {}", message);
 			return message;
